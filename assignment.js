5f19404baae98a9d36b1cb582992e362a5d56f7b
burhanwebworld <burhan102957@gmail.com>
2021-02-24 02:49:50 +0600

    first part done

---
+++ assignment.js
@@ -0,0 +1,20 @@
+function feetToMile(feet){
+    var result = feet / 5280;
+    return result;
+}
+var mile = feetToMile(10560);
+//console.log(mile);
+
+function woodCalculator(chair, table, bed){
+
+}
+
+function brickCalculator(){
+
+}
+
+function tinyFriend(){
+    var Name = ["Burhan", "Masud", "Tushar", "Sohi"];
+    console.log(Name);
+
+}
